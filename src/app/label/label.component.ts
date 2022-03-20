b213c2683a681bd5d027ca2f5165234610ece853
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-03-20 23:00:31 +0100

    Prepare demo

---
+++ src/app/label/label.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-label',
+  template: `
+    <label [for]="for" class="form-label">
+      <ng-content></ng-content>
+      <ng-container *ngIf="required">*</ng-container>
+    </label>
+  `,
+  styles: [],
+})
+export class LabelComponent {
+  @Input() for?: string;
+  @Input() control!: AbstractControl;
+
+  get required(): boolean {
+    return this.control.hasValidator(Validators.required);
+  }
+}
